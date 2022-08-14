1b12407f46efa4edf29e39eea053cd4aa80d99e7
Felix <felix.gaudin@student.uclouvain.be>
2022-08-14 18:55:42 +0200

    correct deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,5 @@ git add -f dist && git commit -m 'adding dist subtree'
 git subtree push --prefix dist origin gh-pages
 
 # source : https://learnvue.co/tutorials/deploy-vue-to-github-pages
+
+git push
